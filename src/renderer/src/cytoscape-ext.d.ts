f3f0cf45763f69dbbcac0d7fbae07d2fab1a8f09
ryansann <ryansann@umich.edu>
2026-06-23 08:43:32 -0700

    updates for graph viz

---
+++ src/renderer/src/cytoscape-ext.d.ts
@@ -0,0 +1,13 @@
+// Layout extensions ship as plain JS without bundled types.
+declare module 'cytoscape-fcose' {
+  const ext: cytoscape.Ext
+  export default ext
+}
+declare module 'cytoscape-layout-utilities' {
+  const ext: cytoscape.Ext
+  export default ext
+}
+declare module 'cytoscape-elk' {
+  const ext: cytoscape.Ext
+  export default ext
+}
